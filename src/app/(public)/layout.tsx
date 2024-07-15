b7d14568420289d421dea1f109d5cd34a27098e2
Harsh Sandhu <nyxfor13days@gmail.com>
2024-07-15 18:16:13 +0530

    Update layout.tsx

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -1,9 +1,14 @@
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+
 interface Props extends React.PropsWithChildren {}
 
 export default function layout({ children }: Props) {
   return (
     <div className="min-h-[100dvh] grid grid-rows-[auto_1fr_auto]">
+      <Header />
       {children}
+      <Footer />
     </div>
   );
 }
