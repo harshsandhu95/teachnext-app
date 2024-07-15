524cba9ddfcf8f01e2f5da6682f1491296857016
Harsh Sandhu <nyxfor13days@gmail.com>
2024-07-15 20:22:36 +0530

    Update Home Page

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,7 +1,32 @@
+import { SendHorizontalIcon } from "lucide-react";
+import { Button } from "@/components/ui/Button";
+import { Input } from "@/components/ui/Input";
+import { Label } from "@/components/ui/Label";
+
 export default function Page() {
   return (
-    <main>
-      <h1>Page</h1>
-    </main>
+    <section className="py-16 container max-w-5xl flex flex-col items-center justify-center gap-y-4 text-center">
+      <h1 className="text-4xl md:text-7xl font-bold">
+        Your <span className="text-primary">AI-powered teaching assistant</span>{" "}
+        to help you in your everyday tasks.
+      </h1>
+
+      <p>Join the waiting list to get early access.</p>
+
+      <form className="w-full max-w-sm flex items-center gap-2">
+        <Label htmlFor="waiting-list" className="sr-only">
+          Join the waiting list
+        </Label>
+        <Input
+          name="waiting-list"
+          type="email"
+          placeholder="johndoe@example.com"
+          className="flex-1"
+        />
+        <Button size="icon">
+          <SendHorizontalIcon size={16} />
+        </Button>
+      </form>
+    </section>
   );
 }
