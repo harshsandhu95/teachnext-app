a5dc91361196a8af42a0d8a79a0a68f84f367f22
Harsh Sandhu <nyxfor13days@gmail.com>
2024-07-15 20:22:19 +0530

    Create Features Page

---
+++ src/app/(public)/@features/page.tsx
@@ -0,0 +1,70 @@
+import {
+  BadgeCentIcon,
+  FileTextIcon,
+  ListTodoIcon,
+  NotebookPenIcon,
+} from "lucide-react";
+
+export default function Features() {
+  const features = [
+    {
+      name: "Monthly Student Reports",
+      description:
+        "Get monthly reports of your students in a single click. No more manual work.",
+      icon: FileTextIcon,
+    },
+    {
+      name: "AI Question Paper Generator",
+      description:
+        "Generate question papers for your students in a few clicks. Save time and effort.",
+      icon: NotebookPenIcon,
+    },
+    {
+      name: "To do list",
+      description:
+        "A simple to-do list that helps you keep track of your tasks. Get notified when a task is due.",
+      icon: ListTodoIcon,
+    },
+    {
+      name: "Free 500 credits for new users",
+      description:
+        "Get 500 credits for free when you sign up. Use them to generate question papers or get student reports.",
+      icon: BadgeCentIcon,
+    },
+  ];
+
+  return (
+    <section className="container py-16">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto max-w-2xl lg:text-center">
+          <h2 className="text-base font-semibold leading-7">Features</h2>
+          <p className="mt-2 text-3xl font-bold tracking-tight sm:text-4xl">
+            Your everyday tasks made easier.
+          </p>
+          <p className="mt-6 text-lg leading-8">
+            A teaching assistant that helps you in your everyday tasks. From
+            generating question papers to managing your to-do list, we&apos;ve
+            got you covered.
+          </p>
+        </div>
+        <div className="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl">
+          <dl className="grid max-w-xl grid-cols-1 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-2 lg:gap-y-16">
+            {features.map((feature) => (
+              <div key={feature.name} className="relative pl-16">
+                <dt className="text-base font-semibold leading-7">
+                  <div className="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg">
+                    <feature.icon aria-hidden="true" size={24} />
+                  </div>
+                  {feature.name}
+                </dt>
+                <dd className="mt-2 text-base leading-7">
+                  {feature.description}
+                </dd>
+              </div>
+            ))}
+          </dl>
+        </div>
+      </div>
+    </section>
+  );
+}
--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -1,13 +1,18 @@
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 
-interface Props extends React.PropsWithChildren {}
+interface Props extends React.PropsWithChildren {
+  features: React.ReactNode;
+}
 
-export default function layout({ children }: Props) {
+export default function layout({ children, features }: Props) {
   return (
     <div className="min-h-[100dvh] grid grid-rows-[auto_1fr_auto]">
       <Header />
-      {children}
+      <main>
+        {children}
+        {features}
+      </main>
       <Footer />
     </div>
   );
