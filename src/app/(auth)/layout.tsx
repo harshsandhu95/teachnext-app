174b4e0e48594a0f51b80dd40d2abd3aebf0224a
Harsh Sandhu <nyxfor13days@gmail.com>
2024-07-15 22:50:11 +0530

    Setup auth

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,14 @@
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+
+interface Props extends React.PropsWithChildren {}
+
+export default function layout({ children }: Props) {
+  return (
+    <div className="min-h-[100dvh] grid grid-rows-[auto_1fr_auto]">
+      <Header />
+      <main className="flex items-center justify-center">{children}</main>
+      <Footer />
+    </div>
+  );
+}
