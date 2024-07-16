f76cf045fa51b596b7df31da695b779383b62cf5
Harsh Sandhu <nyxfor13days@gmail.com>
2024-07-16 16:22:44 +0530

    Create Store and Setup Redux Provider

--- src/components/RootProvider.tsx
+++ src/components/RootProvider.tsx
@@ -1,21 +1,31 @@
 "use client";
 
+import React from "react";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Provider as ReduxProvider } from "react-redux";
 import { ThemeProvider } from "next-themes";
+import { AppStore, makeStore } from "@/lib/store";
 import { TooltipProvider } from "@/components/ui/Tooltip";
 
 interface Props extends React.PropsWithChildren {}
 
 export default function RootProvider({ children }: Props) {
+  const storeRef = React.useRef<AppStore>();
+  if (!storeRef.current) {
+    storeRef.current = makeStore();
+  }
+
   return (
-    <ClerkProvider>
-      <ThemeProvider
-        attribute="class"
-        defaultTheme="light"
-        disableTransitionOnChange
-      >
-        <TooltipProvider>{children}</TooltipProvider>
-      </ThemeProvider>
-    </ClerkProvider>
+    <ReduxProvider store={storeRef.current}>
+      <ClerkProvider>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="light"
+          disableTransitionOnChange
+        >
+          <TooltipProvider>{children}</TooltipProvider>
+        </ThemeProvider>
+      </ClerkProvider>
+    </ReduxProvider>
   );
 }
