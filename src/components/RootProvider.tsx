1ba010b55a29ea3398ee75cfc0301d39effa6cf7
Harsh Sandhu <nyxfor13days@gmail.com>
2024-07-16 12:05:09 +0530

    Add ThemeProvider to RootProvider

--- src/components/RootProvider.tsx
+++ src/components/RootProvider.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { ClerkProvider } from "@clerk/nextjs";
+import { ThemeProvider } from "next-themes";
 import { TooltipProvider } from "@/components/ui/Tooltip";
 
 interface Props extends React.PropsWithChildren {}
@@ -8,7 +9,13 @@ interface Props extends React.PropsWithChildren {}
 export default function RootProvider({ children }: Props) {
   return (
     <ClerkProvider>
-      <TooltipProvider>{children}</TooltipProvider>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="light"
+        disableTransitionOnChange
+      >
+        <TooltipProvider>{children}</TooltipProvider>
+      </ThemeProvider>
     </ClerkProvider>
   );
 }
