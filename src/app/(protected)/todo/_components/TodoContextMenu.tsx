0d99995e30e78a0f38229d7e8d6d81d8ac346147
Harsh Sandhu <nyxfor13days@gmail.com>
2024-07-16 19:12:20 +0530

    Create TodoContextMenu.tsx

---
+++ src/app/(protected)/todo/_components/TodoContextMenu.tsx
@@ -0,0 +1,24 @@
+import {
+  ContextMenuContent,
+  ContextMenuItem,
+  ContextMenuShortcut,
+} from "@/components/ui/ContextMenu";
+import { Trash2Icon } from "lucide-react";
+
+interface Props {
+  id: string;
+  handleRemove: (id: string) => void;
+}
+
+export default function TodoContextMenu({ id, handleRemove }: Props) {
+  return (
+    <ContextMenuContent className="p-4 w-64">
+      <ContextMenuItem onClick={() => handleRemove(id)}>
+        Delete
+        <ContextMenuShortcut>
+          <Trash2Icon size={14} />
+        </ContextMenuShortcut>
+      </ContextMenuItem>
+    </ContextMenuContent>
+  );
+}
