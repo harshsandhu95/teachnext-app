c8a4717ef82574214cb08d76e6a95c122d9a14f1
Harsh Sandhu <nyxfor13days@gmail.com>
2024-07-16 17:36:14 +0530

    Update todo route

--- src/app/(protected)/todo/page.tsx
+++ src/app/(protected)/todo/page.tsx
@@ -1,16 +1,11 @@
-import { Input } from "@/components/ui/Input";
 import TodoList from "./_components/TodoList";
-import { Button } from "@/components/ui/Button";
+import CreateTodo from "./_components/CreateTodo";
 
 export default function Todo() {
   return (
     <section className="mx-auto h-full w-full max-w-2xl grid grid-cols-1 gap-4">
+      <CreateTodo />
       <TodoList />
-
-      <form className="flex justify-center items-center">
-        <Input type="text" placeholder="Add todo" />
-        <Button className="ml-2">Add</Button>
-      </form>
     </section>
   );
 }
