93a5cce61285305fd9bccbce9ac0a4751fb2cefd
Harsh Sandhu <nyxfor13days@gmail.com>
2024-07-16 17:01:17 +0530

    Update store.ts

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -1,8 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
+import todoReducer from "@/lib/features/todo/todoSlice";
 
 export const makeStore = () => {
   return configureStore({
-    reducer: {},
+    reducer: {
+      todo: todoReducer,
+    },
   });
 };
 
