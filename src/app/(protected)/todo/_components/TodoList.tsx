f51470afe7e7bba5dfd114257eb6f557b53e5cc4
Harsh Sandhu <nyxfor13days@gmail.com>
2024-07-16 19:12:38 +0530

    Add Context Menu to Todo list

--- src/app/(protected)/todo/_components/TodoList.tsx
+++ src/app/(protected)/todo/_components/TodoList.tsx
@@ -2,11 +2,17 @@
 
 import { Button } from "@/components/ui/Button";
 import { Checkbox } from "@/components/ui/Checkbox";
+import {
+  ContextMenu,
+  ContextMenuContent,
+  ContextMenuTrigger,
+} from "@/components/ui/ContextMenu";
 import { removeTodo, toggleTodo } from "@/lib/features/todo/todoSlice";
 import { useAppDispatch, useAppSelector } from "@/lib/hooks/useRedux";
 import { cn } from "@/lib/utils";
 import { Trash2Icon } from "lucide-react";
 import React from "react";
+import TodoContextMenu from "./TodoContextMenu";
 
 export default function TodoList() {
   const todos = useAppSelector((state) => state.todo);
@@ -36,28 +42,34 @@ export default function TodoList() {
       ) : (
         <div className="py-2 flex-1 flex flex-col gap-1">
           {todos.map((todo) => (
-            <div
-              key={todo.id}
-              className="group px-4 py-2 flex-1 flex items-center gap-4 hover:bg-background rounded-lg"
-            >
-              <Checkbox
-                checked={todo.completed}
-                onCheckedChange={() => handleCheckedChange(todo.id)}
-              />
-              <p
-                className={cn(["flex-1", todo.completed ? "line-through" : ""])}
-              >
-                {todo.text}
-              </p>
-              <Button
-                size="icon"
-                variant="ghost"
-                onClick={() => handleRemove(todo.id)}
-                className="invisible group-hover:visible"
-              >
-                <Trash2Icon size={16} />
-              </Button>
-            </div>
+            <ContextMenu key={todo.id}>
+              <ContextMenuTrigger>
+                <div className="group px-4 py-2 flex-1 flex items-center gap-4 hover:bg-background rounded-lg">
+                  <Checkbox
+                    checked={todo.completed}
+                    onCheckedChange={() => handleCheckedChange(todo.id)}
+                  />
+                  <p
+                    className={cn([
+                      "flex-1",
+                      todo.completed ? "line-through" : "",
+                    ])}
+                  >
+                    {todo.text}
+                  </p>
+                  <Button
+                    size="icon"
+                    variant="ghost"
+                    onClick={() => handleRemove(todo.id)}
+                    className="invisible group-hover:visible"
+                  >
+                    <Trash2Icon size={16} />
+                  </Button>
+                </div>
+              </ContextMenuTrigger>
+
+              <TodoContextMenu id={todo.id} handleRemove={handleRemove} />
+            </ContextMenu>
           ))}
         </div>
       )}
