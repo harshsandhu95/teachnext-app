110e701776aad33bfdda119e42866396b47a956d
Harsh Sandhu <nyxfor13days@gmail.com>
2024-07-17 18:56:21 +0530

    Update todoSlice.tsx

--- src/lib/features/todo/todoSlice.tsx
+++ src/lib/features/todo/todoSlice.tsx
@@ -1,40 +1,29 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { v4 as uuidv4 } from "uuid";
+import { todo } from "@/app/types";
 
-interface Todo {
-  id: string;
-  text: string;
-  completed: boolean;
-}
-
-const initialState: Todo[] = [];
+const initialState: todo[] = [];
 
 export const todoSlice = createSlice({
   name: "todo",
   initialState,
   reducers: {
-    addTodo: (state, action: PayloadAction<string>) => {
-      const todo = {
-        id: uuidv4(),
-        text: action.payload,
-        completed: false,
-      };
-
-      state.push(todo);
+    setTodos: (state, action: PayloadAction<todo[]>) => {
+      return action.payload;
+    },
+    addTodo: (state, action: PayloadAction<todo>) => {
+      state.push(action.payload);
+    },
+    toggleTodo: (state, action: PayloadAction<string>) => {
+      const todo = state.find((todo) => todo.id === action.payload);
+      if (todo) {
+        todo.completed = !todo.completed;
+      }
     },
     removeTodo: (state, action: PayloadAction<string>) => {
       return state.filter((todo) => todo.id !== action.payload);
     },
-    toggleTodo: (state, action: PayloadAction<string>) => {
-      return state.map((todo) => {
-        if (todo.id === action.payload) {
-          return { ...todo, completed: !todo.completed };
-        }
-        return todo;
-      });
-    },
   },
 });
 
-export const { addTodo, removeTodo, toggleTodo } = todoSlice.actions;
+export const { setTodos, addTodo, toggleTodo, removeTodo } = todoSlice.actions;
 export default todoSlice.reducer;
